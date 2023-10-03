e518719e9f1254b309d911f9d4b31236df67a4c7
yesh-odoo <yesh@odoo.com>
2023-10-03 16:39:27 -0400

    [ADD] List, form and search view.

--- motorcycle_registry/__manifest__.py
+++ motorcycle_registry/__manifest__.py
@@ -15,6 +15,7 @@
         'security/motorcycle_registry_groups.xml',
         'security/ir.model.access.csv',
         'views/motorcycle_registry_menuitems.xml',
+        'views/motorcycle_registry_views.xml',
     ],
     'demo': [
         'demo/motorcycle_registry_demo.xml',
