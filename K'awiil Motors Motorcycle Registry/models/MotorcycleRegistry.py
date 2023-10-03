620d358ed80df9fd628645b4013c188509a4287f
yesh-odoo <yesh@odoo.com>
2023-10-03 09:53:57 -0400

    [ADD] New model Motorcycle registry with corresponding fields

--- K'awiil Motors Motorcycle Registry/models/MotorcycleRegistry.py
+++ K'awiil Motors Motorcycle Registry/models/MotorcycleRegistry.py
@@ -1,5 +1,17 @@
-from odoo import models
+from odoo import models, fields
 
-class MotorcycleRegistry(models.model):
+class MotorcycleRegistry(models.Model):
     _name = "motorcycle.registry"
     _description = "Motorcycle Registry"
+    _rec_name = 'registry_number'
+
+
+    first_name = fields.Char(string="First Name", required=True)
+    last_name = fields.Char(string="Last Name", required=True)
+    license_plate = fields.Char(string="License Plate")
+    date_registry = fields.Date(string="Registry Date")
+    vin = fields.Char(string="Vehical Identity Number")
+    current_mileage = fields.Float(string="Current Milage")
+    certificate_title = fields.Binary(attachment=True)
+    vehicle_image = fields.Image("Vehicle Image", max_width=1920, max_height=1920)
+    registry_number = fields.Char(string="Registry Number", required=True)
